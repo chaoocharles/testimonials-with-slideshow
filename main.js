2d239d9384e9d8195e504a6e2e469004c51ba868
unknown <chaoocharles>
2020-08-03 16:38:30 +0300

    testimonials complete

--- main.js
+++ main.js
@@ -2,28 +2,31 @@ let counter = 1;
 showTestimonials(counter);
 
 document.querySelector(".prev").addEventListener("click", () => {
-  plusTestimonials(-1);
-});
+    computeTestimonials(-1)
+})
 
 document.querySelector(".next").addEventListener("click", () => {
-  plusTestimonials(1);
-});
+    computeTestimonials(1)
+})
 
-function plusTestimonials(t) {
-  showTestimonials((counter += t));
+function computeTestimonials(t) {
+    showTestimonials((counter += t))
 }
 
 function showTestimonials(n) {
   let i;
   let testimonials = document.getElementsByClassName("testimonial");
+
   if (n > testimonials.length) {
     counter = 1;
   }
   if (n < 1) {
     counter = testimonials.length;
   }
-  for (i = 0; i < testimonials.length; i++) {
-    testimonials[i].style.display = "none";
+
+  for( i = 0; i < testimonials.length; i++){
+      testimonials[i].style.display = "none"
   }
-  testimonials[counter - 1].style.display = "flex";
+
+  testimonials[counter - 1].style.display = "flex"
 }
